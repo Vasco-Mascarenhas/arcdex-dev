28f49df0b1c9998124c2324659da385b88e706cb
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-09-25 16:41:30 +0100

    init

---
+++ app/components/container/container.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./container.module.css";
+const Container = ({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) => {
+	return <div className={styles.container}>{children}</div>;
+};
+
+export default Container;
---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+interface Routes {
+	name: string;
+	link: string;
+}
+
+const routes: Routes[] = [
+	{
+		name: "home",
+		link: "/",
+	},
+	{
+		name: "pokedex",
+		link: "/pokedex",
+	},
+	{
+		name: "abilities",
+		link: "/abilities",
+	},
+	{
+		name: "moves",
+		link: "/moves",
+	},
+];
+
+/*
+
+
+*/
+
+const NavBar = () => {
+	const pathName = usePathname();
+	return (
+		<nav className={styles.navbar}>
+			{routes.map((route) => (
+				<Link
+					href={route.link}
+					key={route.name}
+					className={`${styles.link} ${
+						pathName === route.link ? styles.active : ""
+					}`}
+				>
+					<Image
+						className={styles.logo}
+						src={`/navbar/${route.name}.png`}
+						alt={route.name}
+						width={50}
+						height={50}
+					/>
+					{route.name}
+				</Link>
+			))}
+		</nav>
+	);
+};
+
+export default NavBar;
