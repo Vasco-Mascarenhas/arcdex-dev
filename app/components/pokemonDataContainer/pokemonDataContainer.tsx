a02f0dbe7697f8942ea41fb2ddb10b3fe7fd533f
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-09-27 15:13:55 +0100

    replaced pokemons array sorting for pokemonPreview

--- app/components/pokemonDataContainer/pokemonDataContainer.tsx
+++ app/components/pokemonDataContainer/pokemonDataContainer.tsx
@@ -6,20 +6,22 @@ import PokemonPreview from "../pokemon-preview/pokemonPreview";
 import styles from "./pokemonDataContainer.module.css";
 import PokemonData from "../pokemonData/pokemonData";
 import { SearchParams } from "@/app/interfaces/searchParams/searchPara";
+import { PokemonShort } from "@/app/interfaces/pokemons/pokemonShort";
 
 const PokemonDataContainer = ({
 	searchParams,
 }: {
 	searchParams: SearchParams;
 }) => {
-	const pokemon = searchParams.pokemon
-		? pokemons[searchParams.pokemon - 1]
-		: pokemons[1];
+	const pokemon: PokemonShort | undefined = searchParams.pokemon
+		? pokemons.find((p) => p.id === Number(searchParams.pokemon))
+		: pokemons[0];
 
 	console.log(searchParams.pokemon);
 	return (
 		<div className={styles.pokemonDataContainer}>
-			<PokemonPreview pokemon={pokemon} />
+			{pokemon ? <PokemonPreview pokemon={pokemon} /> : <div>error</div>}
+
 			<PokemonData searchParams={searchParams} />
 		</div>
 	);
