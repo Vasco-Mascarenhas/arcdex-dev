d463a455d5bbca6e7ada55df06f3185a950398f2
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-09-30 16:31:39 +0100

    test 4

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./page.module.css";
+import { SearchParams } from "../interfaces/searchParams/searchPara";
 import PokedexContainer from "../components/pokedexContainer/pokedexContainer";
-const Page = () => {
+const Page = ({ searchParams }: { searchParams: SearchParams }) => {
 	console.log("rendered");
 	return (
 		<div className={styles.pokedex}>
-			<PokedexContainer />
+			<PokedexContainer searchParams={searchParams} />
 		</div>
 	);
 };
