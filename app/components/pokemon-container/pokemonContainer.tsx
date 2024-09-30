b3fbd997a54b490c458d314b9bb7d143bdcc58d1
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-09-30 19:11:48 +0100

    add lazy loading

--- app/components/pokemon-container/pokemonContainer.tsx
+++ app/components/pokemon-container/pokemonContainer.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { PokemonShort } from "@/app/interfaces/pokemons/pokemonShort";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./pokemonContainer.module.css";
 import PokemonCard from "../pokemon-card/pokemonCard";
 import { SearchParams } from "@/app/interfaces/searchParams/searchPara";
+
+const INITIAL_LOAD = 30;
+const LOAD_INCREMENT = 30;
 
 interface PokemonContainerProps {
 	pokemons: PokemonShort[];
@@ -13,8 +17,10 @@ const PokemonContainer = ({
 	pokemons,
 	searchParams,
 }: PokemonContainerProps) => {
+	const [visibleCount, setVisibleCount] = useState(INITIAL_LOAD);
 	let filteredPokemons: PokemonShort[] = pokemons;
 
+	// Filter the Pokémon data based on the search query
 	if (searchParams.searched) {
 		filteredPokemons = pokemons?.filter((pokemon) =>
 			pokemon.name.toLowerCase().includes(searchParams.searched)
@@ -26,10 +32,27 @@ const PokemonContainer = ({
 			pokemon.types.some((type) => type.type.name === searchParams.type)
 		);
 	}
-	console.log(searchParams);
+
+	const handleScroll = () => {
+		const scrollPosition =
+			window.innerHeight + document.documentElement.scrollTop;
+		const threshold = document.documentElement.offsetHeight - 100;
+
+		if (scrollPosition >= threshold) {
+			setVisibleCount((prevCount) => prevCount + LOAD_INCREMENT);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className={styles.pokemons}>
-			{filteredPokemons.map((pokemon: PokemonShort) => (
+			{filteredPokemons.slice(0, visibleCount).map((pokemon: PokemonShort) => (
 				<PokemonCard key={pokemon.id} pokemon={pokemon} />
 			))}
 		</div>
