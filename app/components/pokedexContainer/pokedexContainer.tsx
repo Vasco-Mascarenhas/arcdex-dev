535f0e9c22fdc6fd68a5abfba7fed186250797ea
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-09-30 16:18:02 +0100

    test2

--- app/components/pokedexContainer/pokedexContainer.tsx
+++ app/components/pokedexContainer/pokedexContainer.tsx
@@ -1,47 +1,18 @@
 import { typeOptions } from "@/app/constants/typeOptions";
-import { PokemonResponse } from "@/app/interfaces/pokemons/pokemonResponse";
-import { SearchParams } from "@/app/interfaces/searchParams/searchPara";
-import React, { Suspense } from "react";
+import React from "react";
 import Ordering from "../ordering/ordering";
 import PokemonContainer from "../pokemon-container/pokemonContainer";
-import PokemonExpanded from "../pokemonExpanded/pokemonExpanded";
 import styles from "./pokedexContainer.module.css";
 import pokemons from "../../data/pokemons.json";
-const PokedexContainer = async ({
-	searchParams,
-}: {
-	searchParams: SearchParams;
-}) => {
-	let res;
-	if (searchParams.pokemon) {
-		res = await fetch(
-			`https://pokeapi.co/api/v2/pokemon/${searchParams.pokemon}`,
-			{
-				next: { revalidate: 0 }, // Cache indefinitely
-			}
-		);
-	} else {
-		res = await fetch(`https://pokeapi.co/api/v2/pokemon/${1}`, {
-			next: { revalidate: 0 }, // Cache indefinitely
-		});
-	}
-
-	const data: PokemonResponse = await res.json();
-
+const PokedexContainer = () => {
 	return (
 		<>
 			<aside className={styles.pokemonData}></aside>
 			<section className={styles.pokemonSection}>
-				{searchParams.expanded ? (
-					<Suspense fallback={<div>loading..</div>}>
-						<PokemonExpanded pokemonRes={data} />
-					</Suspense>
-				) : (
-					<>
-						<Ordering options={typeOptions} placeholder="Search for Pokemon!" />
-						<PokemonContainer pokemons={pokemons} />
-					</>
-				)}
+				<>
+					<Ordering options={typeOptions} placeholder="Search for Pokemon!" />
+					<PokemonContainer pokemons={pokemons} />
+				</>
 			</section>
 		</>
 	);
--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./page.module.css";
-import { SearchParams } from "../interfaces/searchParams/searchPara";
 import PokedexContainer from "../components/pokedexContainer/pokedexContainer";
-const Page = ({ searchParams }: { searchParams: SearchParams }) => {
+const Page = () => {
 	console.log("rendered");
 	return (
 		<div className={styles.pokedex}>
-			<PokedexContainer searchParams={searchParams} />
+			<PokedexContainer />
 		</div>
 	);
 };
