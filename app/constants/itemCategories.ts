352c720e445b3fe0b113cb1c392283b38972564a
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-10-05 21:26:50 +0100

    removed useless fonts folder, itemcategories options

---
+++ app/constants/itemCategories.ts
@@ -0,0 +1,222 @@
+export const itemCategories = [
+	{
+		value: "",
+		label: "No category",
+	},
+	{
+		value: "stat-boosts",
+		label: "stat boosts",
+	},
+	{
+		value: "effort-drop",
+		label: "effort drop",
+	},
+	{
+		value: "medicine",
+		label: "medicine",
+	},
+	{
+		value: "other",
+		label: "other",
+	},
+	{
+		value: "in-a-pinch",
+		label: "in a pinch",
+	},
+	{
+		value: "picky-healing",
+		label: "picky healing",
+	},
+	{
+		value: "type-protection",
+		label: "type protection",
+	},
+	{
+		value: "baking-only",
+		label: "baking only",
+	},
+	{
+		value: "collectibles",
+		label: "collectibles",
+	},
+	{
+		value: "evolution",
+		label: "evolution",
+	},
+	{
+		value: "spelunking",
+		label: "spelunking",
+	},
+	{
+		value: "held-items",
+		label: "held items",
+	},
+	{
+		value: "choice",
+		label: "choice",
+	},
+	{
+		value: "effort-training",
+		label: "effort training",
+	},
+	{
+		value: "bad-held-items",
+		label: "bad held items",
+	},
+	{
+		value: "training",
+		label: "training",
+	},
+	{
+		value: "plates",
+		label: "plates",
+	},
+	{
+		value: "species-specific",
+		label: "species specific",
+	},
+	{
+		value: "type-enhancement",
+		label: "type enhancement",
+	},
+	{
+		value: "event-items",
+		label: "event items",
+	},
+	{
+		value: "gameplay",
+		label: "gameplay",
+	},
+	{
+		value: "plot-advancement",
+		label: "plot advancement",
+	},
+	{
+		value: "unused",
+		label: "unused",
+	},
+	{
+		value: "loot",
+		label: "loot",
+	},
+	{
+		value: "all-mail",
+		label: "all mail",
+	},
+	{
+		value: "vitamins",
+		label: "vitamins",
+	},
+	{
+		value: "healing",
+		label: "healing",
+	},
+	{
+		value: "pp-recovery",
+		label: "pp recovery",
+	},
+	{
+		value: "revival",
+		label: "revival",
+	},
+	{
+		value: "status-cures",
+		label: "status cures",
+	},
+	{
+		value: "mulch",
+		label: "mulch",
+	},
+	{
+		value: "special-balls",
+		label: "special balls",
+	},
+	{
+		value: "standard-balls",
+		label: "standard balls",
+	},
+	{
+		value: "dex-completion",
+		label: "dex completion",
+	},
+	{
+		value: "scarves",
+		label: "scarves",
+	},
+	{
+		value: "all-machines",
+		label: "all machines",
+	},
+	{
+		value: "flutes",
+		label: "flutes",
+	},
+	{
+		value: "apricorn-balls",
+		label: "apricorn balls",
+	},
+	{
+		value: "apricorn-box",
+		label: "apricorn box",
+	},
+	{
+		value: "data-cards",
+		label: "data cards",
+	},
+	{
+		value: "jewels",
+		label: "jewels",
+	},
+	{
+		value: "miracle-shooter",
+		label: "miracle shooter",
+	},
+	{
+		value: "mega-stones",
+		label: "mega stones",
+	},
+	{
+		value: "memories",
+		label: "memories",
+	},
+	{
+		value: "z-crystals",
+		label: "z crystals",
+	},
+	{
+		value: "species-candies",
+		label: "species candies",
+	},
+	{
+		value: "catching-bonus",
+		label: "catching bonus",
+	},
+	{
+		value: "dynamax-crystals",
+		label: "dynamax crystals",
+	},
+	{
+		value: "nature-mints",
+		label: "nature mints",
+	},
+	{
+		value: "curry-ingredients",
+		label: "curry ingredients",
+	},
+	{
+		value: "tera-shard",
+		label: "tera shard",
+	},
+	{
+		value: "sandwich-ingredients",
+		label: "sandwich ingredients",
+	},
+	{
+		value: "tm-materials",
+		label: "tm materials",
+	},
+	{
+		value: "picnic",
+		label: "picnic",
+	},
+];
