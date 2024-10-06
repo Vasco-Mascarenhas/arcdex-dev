48ba00f9151a13bb844a2926c23a9c23c701751b
Vasco Mascarenhas <vasco.mascarenhas@hotmail.com>
2024-10-06 18:37:20 +0100

    wrapped select in a suspense

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./page.module.css";
 import { itemCategories } from "../constants/itemCategories";
 import Selects from "../components/select/select";
@@ -9,7 +9,9 @@ const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
 	return (
 		<div className={styles.items}>
 			<div className={styles.itemsOrdering}>
-				<Selects options={itemCategories} />
+				<Suspense>
+					<Selects options={itemCategories} />
+				</Suspense>
 			</div>
 
 			<div className={styles.container}>
